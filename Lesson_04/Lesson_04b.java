1e9d52c135717bdcbda37174d7a7a3b4891a399f
mailynnjoyce <EvangelistaM2383@sduhsd.lan>
2016-09-16 12:41:30 -0700

    update

--- Lesson_04/Lesson_04b.java
+++ Lesson_04/Lesson_04b.java
@@ -4,33 +4,32 @@ public class Lesson_04b
 {
 	public static void main(String[]args)
 	{
-		Lesson_04b form= new Lesson_04b();
-		
+		Lesson_04b form = new Lesson_04b();
 		Scanner kb = new Scanner(System.in);
+		
 		System.out.println("Enter First Name:");
 		String first = kb.next();
 		System.out.println("Enter Last Name:");
 		String last = kb.next();
 		System.out.println("Enter Your Title:");
 		String title = kb.next();
+		kb.nextLine();
 		System.out.println("Enter High School:");
-		String hs = kb.next();
+		String hs = kb.nextLine();
 		System.out.println("Enter Graduation Year:");
-		double grad = kb.nextDouble();
+		int grad = kb.nextInt();
 		System.out.println("Enter ID Number");
-		double id = kb.nextDouble();
+		int id = kb.nextInt();
 		
 		form.format(first, last, title, hs, grad, id);
 	}
 	
-	public void format(String first, String last, String title, String hs, double grad, double id)
+	public void format(String first, String last, String title, String hs, int grad, int id)
 	{
-		System.out.println("*********************");
-		
-		System.out.printf("\n%15s  %15.4f", hs, grad);
-		System.out.printf("\n%15s  %15f", first, last);
-		System.out.printf("\n%15s  %15.7f", title, id);
-		
-		System.out.println("\n*********************");
+		System.out.println("********************************");
+		System.out.printf("/n%15s  %15d", hs, grad);
+		System.out.printf("\n%15s  %15s", last, first);
+		System.out.printf("\n%15s  %15d", title, id);
+		System.out.println("\n********************************");
 	}
 }
