10cc322accc3289f8ee4033568d494aed6d03f9b
mailynnjoyce <mailynnevangelista810@gmail.com>
2016-11-29 12:09:29 -0800

    starting 10

---
+++ Lesson_10/Composites.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Composites
+{
+	public static void main (String[]args)
+	{
+		Integer [] numbers = {2, 6, 8, 9, 10, 12, 13, 15, 17, 24, 55, 66, 77, 78, 79};
+		ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(numbers));
+		System.out.println(nums);
+	}
+}
