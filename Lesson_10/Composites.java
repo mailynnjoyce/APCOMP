f96239dbc08eb1b7ea64538812d86063cc370dbb
mailynnjoyce <mailynnevangelista810@gmail.com>
2016-12-01 12:14:50 -0800

    cool

--- Lesson_10/Composites.java
+++ Lesson_10/Composites.java
@@ -7,6 +7,31 @@ public class Composites
 	{
 		Integer [] numbers = {2, 6, 8, 9, 10, 12, 13, 15, 17, 24, 55, 66, 77, 78, 79};
 		ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(numbers));
-		System.out.println(nums);
+		System.out.println("For the following numbers... " + nums);
+		System.out.println("These are the composite numbers... " + removesPrime(nums));
+	}
+	
+	public static int gFactor (int n)
+	{
+		for (int i = 2; i < n; i++) 
+		{
+			if (n%i == 0)
+			{	
+				return 1;
+			}
+		}
+		return 0;
+	}
+	
+	public static ArrayList<Integer> removesPrime (ArrayList<Integer> nums)
+	{
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (gFactor(nums.get(i))==0)
+			{
+				nums.remove(i);
+			}
+		}
+		return nums;
 	}
 }
---
+++ Lesson_10/ExpressionSolver.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ExpressionSolver
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter equation with spaces: ");
+		String expression = kb.nextLine();
+		
+		ArrayList<String> equation = new ArrayList<>(Arrays.asList(expression.split(" ")));
+		System.out.println(doEquation(equation));
+	}
+	
+	public static ArrayList<String> doEquation (ArrayList<String> x)
+	{
+		int i = 0;
+		while (i < x.size())
+		{
+			if (x.get(i).equals("*") || x.get(i).equals("/"))
+			{
+				if (x.get(i).equals("*"))
+				{
+					x.set(i, "" + (Integer.parseInt(x.get(i-1)) * Integer.parseInt(x.get(i+1))));
+				}
+				if (x.get(i).equals("/"))
+				{
+					x.set(i, "" + (Integer.parseInt(x.get(i-1)) / Integer.parseInt(x.get(i+1))));
+				}
+				x.remove(i-1);
+				x.remove(i);
+			}
+			else
+			{
+				i++;
+			}
+		}
+		while (i < x.size())
+		{
+			if (x.get(i).equals("+") || x.get(i).equals("-"))
+			{
+				if (x.get(i).equals("+"))
+				{
+					x.set(i, "" + (Integer.parseInt(x.get(i-1)) + Integer.parseInt(x.get(i+1))));
+				}
+				if (x.get(i).equals("-"))
+				{
+					x.set(i, "" + (Integer.parseInt(x.get(i-1)) - Integer.parseInt(x.get(i+1))));
+				}
+				x.remove(i-1);
+				x.remove(i);
+			}
+			else
+			{
+				i++;
+			}
+		}
+		return x;
+	}
+}
