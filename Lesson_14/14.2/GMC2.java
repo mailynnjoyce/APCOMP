8874467b4c4e5a2f860311adc8273e176b89b7af
mailynnjoyce <mailynnevangelista810@gmail.com>
2017-03-16 11:29:40 -0700

    14.2

---
+++ Lesson_14/14.2/GMC2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class GMC2 extends Car2
+{
+	private double x,y;
+	
+	public GMC2()
+	{
+		x = 0;
+		y = 0;
+	}
+	
+	public GMC2(double x1, double y1)
+	{
+		x = x1;
+		y = y1;
+	}
+	
+	public double[] getLoc()
+	{
+		return (new double[]{x,y});
+	}
+}
---
+++ Lesson_14/14.2/Toyota2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Toyota2 extends Car2
+{
+	private double x,y;
+	
+	public Toyota2()
+	{
+		x = 0;
+		y = 0;
+	}
+	
+	public Toyota2(double x1, double y1)
+	{
+		x = x1;
+		y = y1;
+	}
+	
+	public Toyota2(String coordinates)
+	{
+		String[] parts = coordinates.split(",");
+		x = Double.parseDouble(parts[0]);
+		y = Double.parseDouble(parts[1]);
+	}
+	
+	public Toyota2(double[] coordinates)
+	{
+		x = coordinates[0];
+		y = coordinates[1];
+	}
+	
+	public double[] getLoc()
+	{
+		return (new double[]{x,y});
+	}
+}
