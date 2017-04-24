ccffcd403c4e8b856e9c9d4dd827303ef2fdb012
mailynnjoyce <mailynnevangelista810@gmail.com>
2017-04-24 12:13:02 -0700

    pix

--- PictureLab_Student/classes/PictureExplorer.java
+++ PictureLab_Student/classes/PictureExplorer.java
@@ -800,7 +800,9 @@ public class PictureExplorer implements MouseMotionListener, ActionListener, Mou
    */
   public static void main( String args[])
   {
-    Picture pix = new Picture("beach.jpg");
+    Picture pix = new Picture("NewYork.jpg");
+	Picture smallPix = pix.scale(0.25,0.25);
+	smallPix.write("smallNY.jpg");
     pix.explore();
   }
   
